f1d7d30a59408d03bbe8876c59fa3afd74e648b1
Max Maybury <maxmaybury0509@gmail.com>
2013-04-07 16:41:26 -0400

    Made a bunch of .xml's -MM

--- Smackchat/src/com/smackchat/MainActivity.java
+++ Smackchat/src/com/smackchat/MainActivity.java
@@ -119,10 +119,7 @@ public class MainActivity extends FragmentActivity {
 				Bundle savedInstanceState) {
 			View rootView = inflater.inflate(R.layout.fragment_main_dummy,
 					container, false);
-			TextView dummyTextView = (TextView) rootView
-					.findViewById(R.id.section_label);
-			dummyTextView.setText(Integer.toString(getArguments().getInt(
-					ARG_SECTION_NUMBER)));
+
 			return rootView;
 		}
 	}
